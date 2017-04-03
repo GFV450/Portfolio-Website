6e15fcb1c41ddbdc3bb87644cd7f1f02a36dc41f
GFV450 <gfv450@gmail.com>
2017-04-03 11:24:14 -0700

    Modified NavBar

--- src/components/app.js
+++ src/components/app.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
         <div className="App">
-            // <NavBar />
+            <NavBar />
             <FirstScreen />
             <AboutMe />
             <Projects />
--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react'
 
-export default function(props) {
-    return (
-        <nav className="navBar">
-        <h3 className="navName">Gian Franco Vitola</h3>
-            <ul className="navButtonsBox">
-                <li><a className="navButtons" href="#about">ABOUT ME</a></li>
-                <li><a className="navButtons" href="#projects">PROJECTS</a></li>
-                <li><a className="navButtons" href="#contact">CONTACT</a></li>
-            </ul>
-        </nav>
-    )
+export default class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: "navBar"
+        };
+    }
+    render() {
+        return (
+            <nav className={this.state.color}>
+            <h3 className="navName">Gian Franco Vitola</h3>
+                <ul className="navButtonsBox">
+                    <li><a className="navButtons" href="#about">ABOUT ME</a></li>
+                    <li><a className="navButtons" href="#projects">PROJECTS</a></li>
+                    <li><a className="navButtons" href="#contact">CONTACT</a></li>
+                </ul>
+            </nav>
+        )
+    }
 }
