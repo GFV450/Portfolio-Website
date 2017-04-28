adea871c4c99b56bb1c6d9aa6a362a754076da04
GFV450 <gfv450@gmail.com>
2017-04-28 12:49:34 -0700

    Added Butterfly Knife Fight, LOTR Projects. Added background to contact screen.

--- src/components/projects.js
+++ src/components/projects.js
@@ -16,8 +16,24 @@ export default function(props) {
                 src: "../img/project/iChallenge.png",
                 title: "iChallenge",
                 text: "An app made for challenging your friends to have new experiences and break their day to day routines",
-                buttonRef: "https://itunes.apple.com/us/genre/ios-social-networking/id6005?mt=8",
+                buttonRef: "https://github.com/GFV450/iChallenge",
                 buttonDesc: "In Review"
+            },
+            {
+                projectName: "ButterflyKnifeFight",
+                src: "../img/project/BKF.png",
+                title: "Butterfly Knife Fight",
+                text: "A local multiplayer arena-style combat game, with butterflies... And knives! (in development)",
+                buttonRef: "https://drive.google.com/file/d/0B24DfMDOOKV3cE1nTS1LdjBuU3M/view?usp=sharing",
+                buttonDesc: "Download"
+            },
+            {
+                projectName: "LOTR",
+                src: "../img/project/LOTR.png",
+                title: "LOTR Tweet Gen",
+                text: "A Lord of the Rings tweet generator built with data structures and natural language processing",
+                buttonRef: "https://github.com/GFV450/Tweet-Generator",
+                buttonDesc: "Coming soon"
             }
         ]
 
